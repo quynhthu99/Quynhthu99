e47bb98eefca9988041e54d5280d7f13c7c2ef66
Codese <quynhthuluong@gmail.com>
2020-08-02 13:58:44 +0700

    first commit

--- codefullstack/lesson3/backend/src/index.js
+++ codefullstack/lesson3/backend/src/index.js
@@ -81,3 +81,78 @@ prom.then(
 )
 
 //dong87
+const getData = (number) => {
+    //   return new Promise((resolve, reject) => {
+    //     if (number == 0) reject('Khong chia duoc')
+    //     else
+    //       resolve(100 / number)
+    //   })
+    // }
+    
+    const array = [1, 2, 3, 4, 5, 6, 1, 23, 1, 1, 3, 2, 2]
+    
+    console.log("Hello world");
+    
+    for (let i = 0; i < array.length; i++) {
+      const element = array[i];
+    }
+    
+    console.log(array[3]);
+    
+    const object = {
+      a: 100000,
+      b: "123",
+      c: false
+    }
+    console.log(object['a']);
+    console.log(object.a);
+    
+    const ketquamap = array.map((value, index) => {
+      console.log(value, ' indexing ', index);
+      return value * 100
+    })
+    console.log(ketquamap);
+    // [1, 2, 3] ===== [100, 200, 300]
+    // [1 2 1 1 1 1 1]
+    const ketquafilter = array.filter((value) => {
+      // return true/false
+      if (value < 3) return true;
+      else return false
+    })
+    // [1, 2, 3 ,5 ,6 ,9] == []
+    
+    console.log(ketquafilter);
+    
+    const user = {
+      username: 'thien',
+      tt1: "asdad",
+      tt2: 12,
+      // ..... //
+      active: true
+      // .... //
+    }
+    
+    const user1 = {
+      ...user,
+      active: "dang hoat dong",
+      tt2: 45
+    }
+    
+    console.log(user1);
+    
+    
+    
+    const fun5 = (a, b, c = 3, d, e = 12) => {
+    
+    }
+    
+    fun5(1, 2, 3, 4)
+    
+    const user = {
+      tt1: 1,
+      tt2: 2,
+      tt3: 4,
+    }
+    
+    const user2 = { ...user }
+    const user3 = user2;
